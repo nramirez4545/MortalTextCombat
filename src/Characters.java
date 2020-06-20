c7270c428982c8513142524222dcb189865d43fc
solidpro9 <solidpro9@gmail.com>
2020-06-20 15:10:20 -0500

    Created constructor in the characters class

--- src/Characters.java
+++ src/Characters.java
@@ -1,6 +1,16 @@
 
 public class Characters {
 	
-	
+	private int hp;
+	private String name;
+	private int strength;
+
+	public Characters(int hp, String name, int strength) {
+		
+		this.hp = hp;
+		this.name = name;
+		this.strength= strength;
+				
+	}
 
 }
--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,9 @@ import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) {
+		//Character objects
 		Characters raymond = new Characters(100, "Raymond", 25);
+		Characters nick = new Characters(100, "Nick", 5);
 		Menu menu1 = new Menu();
 		menu1.choices();
 		Scanner scan5 = new Scanner(System.in);
