a6e0672203752b69bd182b9b21c52c39fef35cb4
raymond <raymond@raymond-HP.attlocal.net>
2020-06-20 15:03:12 -0500

    created my character object

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) {
-		
+		Characters raymond = new Characters(100, "Raymond", 25);
 		Menu menu1 = new Menu();
 		menu1.choices();
 		Scanner scan5 = new Scanner(System.in);
