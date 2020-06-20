d2478c72140350e5ad07dc9a9e68432305568834
raymond <raymond@raymond-HP.attlocal.net>
2020-06-20 14:50:19 -0500

    outline of a menu

--- src/Menu.java
+++ src/Menu.java
@@ -1,4 +1,12 @@
 
 public class Menu {
-
+	public Menu() {
+		
+	}
+	public void choices() {
+		System.out.println("1. Attack");
+		System.out.println("2. Open Inventory");
+		System.out.println("3. Save");
+		System.out.println("4. Load");
+	}
 }
